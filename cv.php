9436ac6c9af7685a18f12b12dcdde4abf16f4d75
Sahar <Sahar@DESKTOP-HA6N9T1>
2018-07-04 22:51:41 +0300

    CV update

--- cv.php
+++ cv.php
@@ -10,7 +10,61 @@
 <div class="content-wrap-inner">
 <h2>CV - Sahar Atias</h2>
 <p>Personal information is not listed here, please <a href="/contact" title="Contact me">send me a message</a> for further details.</p>
-<div class="rtl">
+<p id="languages"><a href="javascript:void(0)" onclick="show('en')">English</a> | <a href="javascript:void(0)" onclick="show('he')">עברית</a></p>
+<div id="en">
+<h3>Education</h3>
+<p>
+<strong>11.2016 - today:</strong> BSc. Computer Science, Ashkelon College, given lectures at Luxembourg and Isralog.<br>
+<strong>9.2011 - 6.2013:</strong> Software Development Practical Engineer, Ort Rehovot College.<br>
+<strong>9.2005 - 6.2011:</strong> Planning and Programming of Information Systems, "Masterpiece Class", Ort Ashkelon High School.
+</p>
+<h3>Work Experience</h3>
+<p><strong>2012 - today:</strong> Java programming.</p>
+<ul>
+<li>Full stack development.</li>
+<li>Code organization with SVN / GIT.</li>
+<li>MySQL, SQL Server and Access database integration.</li>
+<li>Software development at Hatzor military base – management of dining hall duties, management and invitation of Reservists and Contractors.</li>
+<li>Outlook signature creator – passed on to two more bases.</li>
+<li>Software for management of Ground Defenses guards duty – receiving a certificate of appreciation from a colonel.</li>
+</ul>
+<p><strong>2012 - today:</strong> Linux-based server management.</p>
+<ul>
+<li>Experiencing with CentOS and Ubuntu.</li>
+<li>Setting up the server to work as LAMP.</li>
+<li>Coping with cyber events using IPTables and CSF.</li>
+<li>Working with software such as: PHPMyAdmin, cPanel, Kloxo.</li>
+<li>Taking care of specific and systemic malfunctions.</li>
+</ul>
+<p><strong>2010 - today:</strong> Website development.</p>
+<ul>
+<li>Full stack development.</li>
+<li>Client: HTML, CSS, JS, Angular, JQuery, Bootstrap.</li>
+<li>Server: PHP, .NET Web API.</li>
+<li>Wide usage of SQL queries using MySQL database.</li>
+<li>Understanding and usage of APIs from big company such as: Microsoft, Google, Facebook.</li>
+<li>Setting up and managing of forums based on: phpBB, IPB, SMF, MyBB, IPS.</li>
+</ul>
+<h3>Military</h3>
+<p><strong>7.2013 - 7.2016:</strong> Network technician at the air force and discharge as First Sergeant.</p>
+<ul>
+<li>Managing and monitoring a network of more than 1500 users and 750 computers.</li>
+<li>Receiving new projects and solving operational problems around the clock.</li>
+<li>Working with command and control stations and operation of Active Directory.</li>
+<li>Punctuality to ensure continuous functioning of the squadrons.</li>
+<li>Working with transmission and networking equipment such as routers and switches.</li>
+<li>Participation in "Juniper Cobra" with the Americans and in the "Protective Edge" operation.</li>
+<li>Appointment as networking team leader.</li>
+</ul>
+<h3>Volunteering</h3>
+<p>
+<strong>2012 - 2018:</strong> Management of Pet Pal non-profit organization.<br>
+<strong>2008 - 2013:</strong> Volunteering at "SOS Pets Ashkelon" NGO and receiving the certificate of social excellence.
+</p>
+<h3>Languages</h3>
+<p><strong>Hebrew:</strong> native language&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>English:</strong> high level</p>
+</div>
+<div id="he" class="rtl" style="display:none">
 <h3>השכלה</h3>
 <p>
 <strong>11.2016 - היום:</strong> BSc מדעי המחשב במכללת אשקלון, מתן הרצאה בלוקסמבורג וישראלוג.<br>
@@ -29,20 +83,19 @@
 </ul>
 <p><strong>2012 - היום:</strong> ניהול שרתים מבוססים Linux.</p>
 <ul>
-<li>היכרות עם מערכות הפעלה: CentOS, Ubuntu, Debian.</li>
+<li>היכרות עם מערכות הפעלה: CentOS, Ubuntu.</li>
 <li>הגדרת השרת לעבודה כמערכת LAMP.</li>
 <li>התמודדות מול אירועי סייבר באמצעות CSF, IPTables.</li>
 <li>תפעול תוכנות כגון cPanel, Kloxo, PHPMyAdmin.</li>
-<li>טיפול בתקלות נקידתיות ומערכתיות.</li>
-<li>תחזוקה ומתן מענה לפניות של לקוחות.</li>
+<li>טיפול בתקלות נקודתיות ומערכתיות.</li>
 </ul>
 <p><strong>2010 - היום:</strong> בניית אתרים.</p>
 <ul>
-<li>צד לקוח: HTML 4.01 Strict, HTML 5, CSS 2.1, CSS 3, JS, JQuery, AJAX.</li>
-<li>צד שרת: PHP, ASP.</li>
-<li>שימוש נרחב בשאילתות SQL ומסדי נתונים MySQL, Access.</li>
+<li>צד לקוח: HTML, CSS, JS, Angular, JQuery, Bootstrap.</li>
+<li>צד שרת: PHP, .NET Web API.</li>
+<li>שימוש נרחב בשאילתות SQL ובמסד הנתונים MySQL.</li>
 <li>הבנה ושימוש ב-APIs של החברות הגדולות Facebook, Google, Microsoft.</li>
-<li>תפעול וניהול פורומים המבוססים על: MyBB, SMF, IPB, phpBB.</li>
+<li>תפעול וניהול פורומים המבוססים על: MyBB, SMF, IPB, phpBB, IPS.</li>
 </ul>
 <h3>שירות צבאי</h3>
 <p><strong>7.2013 - 7.2016:</strong> טכנאי תקשו"ב בחיל האוויר ושחרור בדרגת סמל ראשון.</p>
@@ -57,7 +110,7 @@
 </ul>
 <h3>התנדבות</h3>
 <p>
-<strong>2012 - היום:</strong> בניית אתר פט פאל ורשימתו כעמותה ללא מטרות רווח.<br>
+<strong>2012 - 2018:</strong> פתיחת אתר פט פאל ורשימתו כעמותה ללא מטרות רווח.<br>
 <strong>2008 - 2013:</strong> התנדבות בעמותת "אס.או.אס חיות אשקלון" וקבלת תעודת בגרות חברתית.
 </p>
 <h3>שפות</h3>
@@ -68,5 +121,17 @@
 </main>
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
 </div>
+<script>
+var ids = new Array('en', 'he');
+function hideAll() {
+    for (var i = 0;i < ids.length;i++)
+        if (document.getElementById(ids[i]))
+            document.getElementById(ids[i]).style.display = 'none';
+}
+function show(id) {
+	hideAll();
+	document.getElementById(id).style.display = 'block';
+}
+</script>
 </body>
 </html>
