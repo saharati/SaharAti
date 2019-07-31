747a3ec35a46ce02b4335566a31d9a7258d8c5e5
saharati <sahar_ati@hotmail.com>
2019-07-31 23:19:23 +0300

    CV update.

--- cv.php
+++ cv.php
@@ -10,40 +10,41 @@
 <div class="content-wrap-inner">
 <h2>CV - Sahar Atias</h2>
 <p>Personal information is not listed here, please <a href="/contact" title="Contact me">send me a message</a> for further details.</p>
-<p id="languages"><a href="javascript:void(0)" onclick="show('en')">English</a> | <a href="javascript:void(0)" onclick="show('he')">עברית</a></p>
-<div id="en">
 <h3>Education</h3>
 <p>
-<strong>11.2016 - today:</strong> BSc. Computer Science, Ashkelon College, given lectures at Luxembourg and Isralog.<br>
+<strong>11.2016 - 8.2019:</strong> BSc. Computer Science, Ashkelon College, lectured at Luxembourg and Isralog.<br>
 <strong>9.2011 - 6.2013:</strong> Software Development Practical Engineer, Ort Rehovot College.<br>
-<strong>9.2005 - 6.2011:</strong> Planning and Programming of Information Systems, "Masterpiece Class", Ort Ashkelon High School.
+<strong>9.2005 - 6.2011:</strong> Planning & Programming of Information Systems, "Masterpiece Class", Ort Ashkelon.
 </p>
 <h3>Work Experience</h3>
 <p><strong>2012 - today:</strong> Java programming.</p>
 <ul>
-<li>Full stack development.</li>
+<li>Backend private game server development.</li>
 <li>Code organization with SVN / GIT.</li>
-<li>MySQL and SQL Server database integration.</li>
-<li>Software development at Hatzor military base – management of dining hall duties, management and invitation of Reservists and Contractors.</li>
-<li>Outlook signature creator – passed on to two more bases.</li>
-<li>Software for management of Ground Defenses guards duty – receiving a certificate of appreciation from a colonel.</li>
-</ul>
-<p><strong>2012 - today:</strong> Linux-based server management.</p>
-<ul>
-<li>Experiencing with CentOS and Ubuntu.</li>
+<li>MySQL database integration.</li>
+<li>Experiencing with CentOS.</li>
 <li>Setting up the server to work as LAMP.</li>
 <li>Coping with cyber events using IPTables and CSF.</li>
-<li>Working with software such as: PHPMyAdmin, cPanel, Kloxo.</li>
+<li>Working with software such as PHPMyAdmin and VestaCP.</li>
 <li>Taking care of specific and systemic malfunctions.</li>
+<li>Software development at Hatzor military base:
+<ol>
+<li>Management of dining hall duties.</li>
+<li>Management and invitation of Reservists and Contractors.</li>
+<li>Outlook signature creator – passed on to two more bases.</li>
+<li>Management of Ground Defense guards' duties – receiving a certificate of appreciation from a colonel.</li>
+</ol>
+</li>
 </ul>
 <p><strong>2010 - today:</strong> Website development.</p>
 <ul>
 <li>Full stack development.</li>
-<li>Client: HTML, CSS, JS and JQuery.</li>
+<li>Client: HTML5, CSS3, JS/JQuery.</li>
 <li>Server: PHP.</li>
 <li>Wide usage of SQL queries using MySQL database.</li>
-<li>Understanding and usage of APIs from big company such as: Microsoft, Google, Facebook.</li>
-<li>Setting up and managing of forums based on: phpBB, IPB, SMF, MyBB, IPS.</li>
+<li>Took a course on AngularJS / Bootstrap / .NET Rest API / NodeJS.</li>
+<li>Usage of APIs from big company such as Google, Facebook, Twitch and Paypal.</li>
+<li>Setting up and managing forums based on phpBB, IPB, SMF, MyBB and IPS.</li>
 </ul>
 <h3>Military</h3>
 <p><strong>7.2013 - 7.2016:</strong> Network technician at the air force and discharge as First Sergeant.</p>
@@ -62,76 +63,11 @@
 <strong>2008 - 2013:</strong> Volunteering at "SOS Pets Ashkelon" NGO and receiving the certificate of social excellence.
 </p>
 <h3>Languages</h3>
-<p><strong>Hebrew:</strong> native language&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>English:</strong> high level</p>
-</div>
-<div id="he" class="rtl" style="display:none">
-<h3>השכלה</h3>
-<p>
-<strong>11.2016 - היום:</strong> BSc מדעי המחשב במכללת אשקלון, מתן הרצאה בלוקסמבורג וישראלוג.<br>
-<strong>9.2011 - 6.2013:</strong> הנדסאי תוכנה, מכללת אורט רחובות.<br>
-<strong>9.2005 - 6.2011:</strong> בגרות מלאה, כיתת מופת ומגמת תכנון ותכנות מערכות מידע, אורט אשקלון.
-</p>
-<h3>ניסיון תעסוקתי</h3>
-<p><strong>2012 - היום:</strong> תכנות בשפת Java.</p>
-<ul>
-<li>צד שרת, צד לקוח והתקשורת בינהם.</li>
-<li>ניהול וארגון קוד באמצעות GIT / SVN.</li>
-<li>בניית תוכנות בבסיס חצור - ניהול תורני חדר אוכל וניהול וזימון אנשי מילואים וקבלנים.</li>
-<li>תוכנה ליצירת חתימות ל-Outlook - עברה ל-2 בסיסים נוספים בחיל האוויר.</li>
-<li>תוכנה לניהול תורנויות הגנה קרקעית - קבלת תעודת הערכה בדרגת אלוף משנה.</li>
-<li>ממשוק התוכנות למסדי נתונים MySQL / SQL Server.</li>
-</ul>
-<p><strong>2012 - היום:</strong> ניהול שרתים מבוססים Linux.</p>
-<ul>
-<li>היכרות עם מערכות הפעלה: CentOS, Ubuntu.</li>
-<li>הגדרת השרת לעבודה כמערכת LAMP.</li>
-<li>התמודדות מול אירועי סייבר באמצעות CSF, IPTables.</li>
-<li>תפעול תוכנות כגון cPanel, Kloxo, PHPMyAdmin.</li>
-<li>טיפול בתקלות נקודתיות ומערכתיות.</li>
-</ul>
-<p><strong>2010 - היום:</strong> בניית אתרים.</p>
-<ul>
-<li>צד לקוח: HTML, CSS, JS, JQuery.</li>
-<li>צד שרת: PHP.</li>
-<li>שימוש נרחב בשאילתות SQL ובמסד הנתונים MySQL.</li>
-<li>הבנה ושימוש ב-APIs של החברות הגדולות Facebook, Google, Microsoft.</li>
-<li>תפעול וניהול פורומים המבוססים על: MyBB, SMF, IPB, phpBB, IPS.</li>
-</ul>
-<h3>שירות צבאי</h3>
-<p><strong>7.2013 - 7.2016:</strong> טכנאי תקשו"ב בחיל האוויר ושחרור בדרגת סמל ראשון.</p>
-<ul>
-<li>מעקב וניטור תקשורת וניהול רשת של מעל 1500 משתמשים ו-750 מחשבים.</li>
-<li>קבלת פרוייקטים חדשים ופתרון תקלות מבצעיות מסביב לשעון.</li>
-<li>עבודה מול עמדות שליטה ובקרה ותפעול של Active Directory.</li>
-<li>עמידה בזמנים על מנת לוודא תפקוד רציף ושוטף של הטייסות.</li>
-<li>עבודה עם ציוד תקשורת ותמסורת לרבות נתבים, מתגים, טלפוניה ועוד.</li>
-<li>השתתפות בתרגילי "Juniper Cobra" עם האמריקאים ובמצבע צוק איתן.</li>
-<li>מינוי לתפקיד ראש צוות תקשורת ומפקד גף תורן.</li>
-</ul>
-<h3>התנדבות</h3>
-<p>
-<strong>2012 - 2018:</strong> פתיחת אתר פט פאל ורשימתו כעמותה ללא מטרות רווח.<br>
-<strong>2008 - 2013:</strong> התנדבות בעמותת "אס.או.אס חיות אשקלון" וקבלת תעודת בגרות חברתית.
-</p>
-<h3>שפות</h3>
-<p><strong>עברית:</strong> שפת אם&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>אנגלית:</strong> רמה גבוהה</p>
-</div>
+<p><strong>Hebrew:</strong> native proficiency&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>English:</strong> professional proficiency</p>
 </div>
 </div>
 </main>
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
 </div>
-<script>
-var ids = new Array('en', 'he');
-function hideAll() {
-    for (var i = 0;i < ids.length;i++)
-        if (document.getElementById(ids[i]))
-            document.getElementById(ids[i]).style.display = 'none';
-}
-function show(id) {
-	hideAll();
-	document.getElementById(id).style.display = 'block';
-}
-</script>
 </body>
 </html>
